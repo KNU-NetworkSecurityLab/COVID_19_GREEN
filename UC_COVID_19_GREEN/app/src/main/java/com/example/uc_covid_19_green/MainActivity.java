cd60ecc23e7d361b3d66e2f50885c15ae047687b
Uknow42 <yoon986763@gmail.com>
2021-09-26 13:06:07 +0900

    카카오 맵 api 추가 및 기타 리소스 추가 작업

--- UC_COVID_19_GREEN/app/src/main/java/com/example/uc_covid_19_green/MainActivity.java
+++ UC_COVID_19_GREEN/app/src/main/java/com/example/uc_covid_19_green/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.uc_covid_19_green;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.security.AlgorithmParameterGenerator;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_location;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_location = findViewById(R.id.tv_location);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        onLocate();
+    }
+
+    public void onLocate() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getApplicationContext(), "권한 필요", Toast.LENGTH_LONG).show();
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                String proivder = location.getProvider();
+                double longitude = location.getLongitude(); //위도
+                double latitude = location.getLatitude(); //경도
+                double altitude = location.getAltitude(); //고도
+
+                Log.d("Location", longitude + " " + latitude);
+            }
+        };
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 1, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 1, locationListener);
+
+
     }
 }
