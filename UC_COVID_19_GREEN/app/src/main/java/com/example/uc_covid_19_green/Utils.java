3e5af10e02d94a01f62364cf3a4318f1048e537e
Uknow42 <yoon986763@gmail.com>
2021-10-30 15:42:22 +0900

    유틸에 서버 정보 불러오기 기능 추가

--- UC_COVID_19_GREEN/app/src/main/java/com/example/uc_covid_19_green/Utils.java
+++ UC_COVID_19_GREEN/app/src/main/java/com/example/uc_covid_19_green/Utils.java
@@ -9,14 +9,18 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
 
 public class Utils {
@@ -68,5 +72,48 @@ public class Utils {
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 1, locationListener);
 
         return strAddress[0];
+    }
+
+    class GetList extends AsyncTask<String, Void, String> {
+        String str = "";
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                URL url = Utils.getListURL();
+                URLConnection con = url.openConnection();
+
+                BufferedReader rd = null;
+                rd = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+
+                String line = "";
+
+                str = rd.readLine();
+                while ((line = rd.readLine()) != null) {
+                    str = line;
+                    //주의 : str = str + line 으로 받을 경우 앞의 쓰레기값까지 싹 다 받음
+                    //어차피 return 되는 jsonarray는 한줄로 싹다 출력되기에 마지막줄만 받으면 됨
+                }
+                //서버에서 return 해준 jsonarray값이 str에 저장됨
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            ;
+
+            return str;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+        }
     }
 }
